31bb494d06e51a6d3208deec859d3420f22fe2b5
harshvarshney2004 <harsh.varshney.ug22@nsut.ac.in>
2025-06-18 15:15:22 +0530

    commit

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import Heading from "../subcomponents/Heading";
+import Subheading from "../subcomponents/Subheading";
+import Button from "../subcomponents/Button";
+import InputBox from "../subcomponents/InputBox";
+import ButtonWarning from "../subcomponents/ButtonWarning";
 
 const Signup = () => {
   return <>Signup</>;
