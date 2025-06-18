82e41f1d114b054b15200212fa4700a5ba77516b
harshvarshney2004 <harsh.varshney.ug22@nsut.ac.in>
2025-06-18 19:19:52 +0530

    3

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Appbar from "../dashboard_components/Appbar";
+import Balance from "../dashboard_components/Balance";
+import { Users } from "../dashboard_components/Users";
 
 const Dashboard = () => {
-  return <div>dashboard</div>;
+  return (
+    <div>
+      <Appbar></Appbar>
+      <Balance></Balance>
+      <Users />
+    </div>
+  );
 };
 
 export default Dashboard;
