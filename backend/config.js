65e9c1341f79b526aaee150ba2bc04e5208173e9
harshvarshney2004 <harsh.varshney.ug22@nsut.ac.in>
2025-06-18 10:51:17 +0530

    commit-2

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,22 @@
+const mongoose = require("mongoose");
+const Account = require("./db");
+
+const moneyTransfer = async (accountIdFrom, accountIdTo, amount) => {
+  try {
+    await Account.findByIdAndUpdate(accountIdFrom, {
+      $inc: { balance: -amount },
+    });
+
+    await Account.findByIdAndUpdate(accountIdTo, {
+      $inc: { balance: +amount },
+    });
+  } catch (error) {
+    console.log("Error in trancation");
+    console.log(error);
+  }
+};
+
 module.exports = {
   JWT_SECRET: "TERI_MAA_KI_JAI_HO_69_96",
+  moneyTransfer,
 };
