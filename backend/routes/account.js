36c70a7b56062bc8fe5a21aacd2c3125cc94b662
harshvarshney2004 <harsh.varshney.ug22@nsut.ac.in>
2025-06-18 11:02:49 +0530

    accountRoute

---
+++ backend/routes/account.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const { Account, User } = require("../db");
+
+module.exports = {
+  router,
+};
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const userRouter = require("./user");
 
 router.use("/user", userRouter.router);
+router.use("/account", accountRouter.router);
 module.exports = {
   router,
 };
